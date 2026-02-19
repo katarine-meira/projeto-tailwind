490fb8d5a1ceeccaf3e71c3c1bd87ed57d862db2
Katarine-meira <katarine.kmo@gmail.com>
2026-02-19 12:12:18 -0300

    feat: update testimonials and calendar feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import { CalendarFeature } from './components/CalendarFeature/index.jsx'
+import { Testimonials } from './components/Testimonials/index.jsx'
 function App() {
 
   return (
-    <>
-      <h1 className='text-blue-500'>Hello Worlddd</h1>
-
-    </>
+    <div className='bg-dark-background'>
+      <Testimonials></Testimonials>
+      <CalendarFeature></CalendarFeature>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ export default {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                graphik: ['"Graphik"', 'sans-serif'],
+                'graphik-bold': ['"Graphik Bold"', 'sans-serif'],
+            },
+            colors: {
+                primary: '#2091F9',
+                text: '#251B42',
+                secondaryText: '#374754',
+                darkBackground: '#252B42',
+            },
+            screens: {
+                'xs': '400px'
+            }
+        },
     },
     plugins: [],
 }
