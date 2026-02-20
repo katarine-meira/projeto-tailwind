0a4697c41fa3870213205994c5d4c529b03ba88c
Katarine-meira <katarine.kmo@gmail.com>
2026-02-20 11:46:21 -0300

    feat: update testimonials

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -6,13 +6,12 @@ import IbmLogo from "../../assets/images/brands/ibm-logo.png"
 export const Testimonials = () => {
     return (
         <section className="bg-white">
-            <div className="w-full max-w-[360px] mx-auto flex flex-col items-center justify-center gap-[80px]">
+            <div className="w-full max-w-[360px] md:max-w-[900px] mx-auto flex flex-col items-center justify-center gap-[80px] md:px-2 xl:px-0">
                 <Heading type="h2" className="w-fit">Testimonials</Heading>
-                <div className="flex flex-col py-4 items-center justify-center gap-11">
+                <div className="flex flex-col py-4 items-center justify-center gap-11 md:gap-12">
                     <img src={IbmLogo} alt="" />
-                    <p className="text-center text-sm text-secondary-text font-bold w-full max-w-[280px]">Most calendars are designed for teams. Slate is designed for freelancers
+                    <p className="text-center text-sm text-secondary-text font-bold w-full max-w-[280px] md:max-w-none md:text-2xl md:font-normal">Most calendars are designed for teams. Slate is designed for freelancers
                         who want a simple way to plan their schedule.</p>
-
                     <div className="flex gap-3 justify-center items-center">
                         <img src={TestimonialsAvatar} alt="" />
                         <div>
